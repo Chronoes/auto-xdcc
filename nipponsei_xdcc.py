49478e89567aa4ed5fd8f67e24fcdca881ba8916
Oosran <oosran@gmail.com>
2018-04-15 13:20:39 +0200

    Changed how the nipponsei packlist is parsed in prep for more changes

--- nipponsei_xdcc.py
+++ nipponsei_xdcc.py
@@ -1,9 +1,10 @@
+# pylint: disable=E0401
 import hexchat
-import requests
-from re import sub as rx
+import requests, re
+from bs4 import BeautifulSoup
 
 __module_name__ = "Nip-XDCC Downloader"
-__module_version__ = "0.1"
+__module_version__ = "0.2"
 __module_description__ = "Handles the Nipponsei music packlist. Works in tandem with the Auto-XDCC Downloader plugin."
 __author__ = "Oosran"
 
@@ -12,6 +13,8 @@ __author__ = "Oosran"
 #       This is the URL of a relevant XDCC packlist.
 p_url = "https://nipponsei.minglong.org/packlist/distro/"
 server_name = "Rizon"
+b_reg = re.compile('[\<\>\w\s\"\*,=]+\/msg\s([\w\|]+)[\s\w\"\*#]+')
+p_reg = re.compile('[\<\>\w+]+#(\d+)[\<\>\/\"\w+\s=]+\[\s?(\w+)\][\<\>\/\w+]+(\[\w+\][\[\]\w+\s-]+.zip)')
 #   END OF MODIFIABLE VARIABLES
 #--------------------------------------
 
@@ -27,19 +30,11 @@ def nprint(line):
 
 def get_list_cb(word, word_eol, userdata):
     r = requests.get(p_url)
-    rs = [l for l in r.text.split("<td>") if not l.startswith('<')]
-    rs = [l for l in rs if "zip" in l or '#' in l]
-    rs = [l.split('<')[0] for l in rs]
-    rs = [l.replace('\n',' ') for l in rs if l]
-    i = 0
-    rf = []
-    while i < len(rs)-1:
-        rf.append(rs[i]+' '+rs[i+1])
-        i += 2
+    bs = BeautifulSoup(r.text, 'html.parser').find(class_='boxRight').find('table').find_all('tr')
 
-    pprint('Listing Nippon|minglong packlist')
-    for l in rf:
-        nprint(l)
+    pprint('Listing {} packlist'.format(b_reg.findall(str(bs))[0]))
+    for n in p_reg.findall(str(bs)):
+        nprint("#{:<3d} {} ({})".format(int(n[0]), n[2], n[1]))
 
     return hexchat.EAT_ALL
 
